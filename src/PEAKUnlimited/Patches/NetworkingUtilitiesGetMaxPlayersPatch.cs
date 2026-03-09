62ba14ab13864080496f38a9d1b5e9776eed3aa0
glarmer <hallrudfordlewis@gmail.com>
2026-03-09 18:40:12 +0000

    Add comment

--- src/PEAKUnlimited/Patches/NetworkingUtilitiesGetMaxPlayersPatch.cs
+++ src/PEAKUnlimited/Patches/NetworkingUtilitiesGetMaxPlayersPatch.cs
@@ -5,7 +5,12 @@ namespace PEAKUnlimited.Patches;
 
 public class NetworkingUtilitiesGetMaxPlayersPatch
 {
-    
+    /* This patch should be all thats needed to change the max player count.
+     
+     In the past max players was a variable that could be changed, then for a period it was a constant.
+     In case this getter is not used in all situations or changes in the future, I have opted to keep the patches
+     from when it was a constant as well as adding this. In the future, I will do testing and remove redundant patches.
+     */
     [HarmonyPatch(typeof(NetworkingUtilities), nameof(NetworkingUtilities.MAX_PLAYERS), MethodType.Getter)]
     [HarmonyPrefix]
     static bool Prefix(ref int __result)
