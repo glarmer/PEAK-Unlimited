92d731e065af640364fedf76260175d9974bd124
glarmer <hallrudfordlewis@gmail.com>
2026-04-11 02:33:26 +0100

    Possibly fix names being jumbled ontop of heads

---
+++ src/PEAKUnlimited/Patches/UIPlayerNamesPatches.cs
@@ -0,0 +1,75 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace PEAKUnlimited.Patches;
+
+[HarmonyPatch(typeof(UIPlayerNames))]
+class UIPlayerNamesPatches
+{
+
+    private static void FixArrayLengths(UIPlayerNames instance)
+    {
+        int targetSize = Plugin.ConfigurationHandler.MaxPlayers;
+        if (instance.playerNameText == null || instance.playerNameText.Length >= targetSize)
+            return;
+
+        var oldArray = instance.playerNameText;
+        int oldLength = oldArray.Length;
+
+        PlayerName template = oldArray[0];
+        Transform parent = template.transform.parent;
+
+        PlayerName[] newArray = new PlayerName[targetSize];
+        
+        for (int i = 0; i < oldLength; i++)
+            newArray[i] = oldArray[i];
+        
+        for (int i = oldLength; i < targetSize; i++)
+        {
+            PlayerName clone = Object.Instantiate(template, parent);
+            clone.gameObject.SetActive(false);
+            
+            clone.group.alpha = 0f;
+            clone.audioImageTimeout = 0f;
+
+            newArray[i] = clone;
+        }
+
+        instance.playerNameText = newArray;
+    }
+    
+    [HarmonyPatch(nameof(UIPlayerNames.Init))]
+    [HarmonyPrefix]
+    static void InitPrefix(UIPlayerNames __instance)
+    {
+        FixArrayLengths(__instance);
+    }
+    
+    [HarmonyPatch(nameof(UIPlayerNames.UpdateName))]
+    [HarmonyPrefix]
+    static bool UpdateNamePrefix(UIPlayerNames __instance, int index)
+    {
+        FixArrayLengths(__instance);
+
+        if (__instance.playerNameText == null)
+            return false;
+
+        if (index >= __instance.playerNameText.Length)
+            return false;
+
+        return true;
+    }
+    
+    [HarmonyPatch(nameof(UIPlayerNames.DisableName))]
+    [HarmonyPrefix]
+    static bool DisableNamePrefix(UIPlayerNames __instance, int index)
+    {
+        if (__instance.playerNameText == null)
+            return false;
+
+        if (index >= __instance.playerNameText.Length)
+            return false;
+
+        return true;
+    }
+}
